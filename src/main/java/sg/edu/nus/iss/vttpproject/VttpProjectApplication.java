c7e7a87094db175d719e87014941d2ef4671b7fd
tngye <tngyeefong@gmail.com>
2022-05-08 21:49:08 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/vttpproject/VttpProjectApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttpproject/VttpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
